6a74677a310cac56e2ff1f113b7f643efa1f0f9e
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-02-03 13:08:55 +0100

    Define constructor for TextureApp class

---
+++ src/com/jtuto/j2dtuto/TextureTutorial.java
@@ -0,0 +1,58 @@
+package com.jtuto.j2dtuto;
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+// *****
+class TextureApp extends JPanel {
+    private BufferedImage slate;
+    private BufferedImage java;
+    private BufferedImage pane;
+    private TexturePaint slatetp;
+    private TexturePaint javatp;
+    private TexturePaint panetp;
+
+    // ---
+    public TextureApp() {
+        loadImages();
+    }
+
+    // ---
+    private void loadImages() {
+    }
+
+    // ---
+    private void doDrawing(Graphics g) {
+    }
+
+    // ---
+    @Override
+    public void paintComponent(Graphics g) {
+    }
+}
+
+// *****
+public class TextureTutorial extends JFrame {
+    public TextureTutorial() {
+    }
+
+    // ---
+    private void initUI() {
+    }
+
+    // ---
+    public static void main(String[] args) {
+
+    }
+}
