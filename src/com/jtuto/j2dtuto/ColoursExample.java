b8d290102a9530129253bde4513fb3d724991695
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-02-03 12:21:53 +0100

    Learn about color primitives

---
+++ src/com/jtuto/j2dtuto/ColoursExample.java
@@ -0,0 +1,58 @@
+package com.jtuto.j2dtuto;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+// ******
+class ColourApp extends JPanel {
+    public void doDrawing(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(125, 167, 116));
+        g2d.fillRect(10, 10, 90, 60);
+
+        g2d.setColor(new Color(42, 179, 231));
+        g2d.fillRect(130, 10, 90, 60);
+
+        g2d.setColor(new Color(70, 67, 123));
+        g2d.fillRect(250, 10, 90, 60);
+
+        g2d.setColor(new Color(130, 100, 84));
+        g2d.fillRect(10, 100, 90, 60);
+
+        g2d.setColor(new Color(252, 211, 61));
+        g2d.fillRect(130, 100, 90, 60);
+
+        g2d.setColor(new Color(241, 98, 69));
+        g2d.fillRect(250, 100, 90, 60);
+
+        g2d.setColor(new Color(217, 146, 154));
+        g2d.fillRect(10, 190, 90, 60);
+
+        g2d.setColor(new Color(63, 121, 186));
+        g2d.fillRect(130, 190, 90, 60);
+
+        g2d.setColor(new Color(31, 21, 1));
+        g2d.fillRect(250, 190, 90, 60);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+    }
+}
+
+// ******
+public class ColoursExample extends JFrame {
+    public ColoursExample() {
+    }
+
+    private void initUI() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
