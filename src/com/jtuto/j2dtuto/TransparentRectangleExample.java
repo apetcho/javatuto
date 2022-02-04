4e8a50e2427f95f3f2d4cf62f7757dc19f24eccb
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-02-04 19:32:51 +0100

    Define constructor for TransparentRectangleExample class

--- src/com/jtuto/j2dtuto/TransparentRectangleExample.java
+++ src/com/jtuto/j2dtuto/TransparentRectangleExample.java
@@ -35,6 +35,7 @@ class Application extends JPanel {
 //
 public class TransparentRectangleExample extends JFrame {
     public TransparentRectangleExample() {
+        initUI();
     }
 
     private void initUI() {
