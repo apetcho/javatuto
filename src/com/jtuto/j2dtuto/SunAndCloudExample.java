8d3fee1bb899d8bde7c73198754451903cedc89f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-02-04 21:24:14 +0100

    Learn composition in this animation

---
+++ src/com/jtuto/j2dtuto/SunAndCloudExample.java
@@ -0,0 +1,64 @@
+package com.jtuto.j2dtuto;
+
+import java.awt.AlphaComposite;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+// ---
+class Application extends JPanel implements ActionListener {
+    //
+    // ****
+    public Application() {
+    }
+
+    // ****
+    private void loadImages() {
+    }
+
+    // ****
+    private void initTimer() {
+    }
+
+    // ****
+    private void doDrawing(Graphics g) {
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+    }
+
+    // ****
+    private void step() {
+    }
+
+    // ****
+    @Override
+    public void actionPerformed(ActionEvent event) {
+    }
+}
+
+// ---
+public class SunAndCloudExample extends JFrame {
+
+    // *****
+    public SunAndCloudExample() {
+    }
+
+    // *****
+    private void initUI() {
+    }
+
+    // *****
+    public static void main(String[] args) {
+
+    }
+}
